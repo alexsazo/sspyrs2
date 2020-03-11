58516cea5035fda44e1e2d8d98f631b61f22541f
alexsazo <alexsazoster@gmail.com>
2020-03-11 12:56:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup
 
-setup(name='sspyrs',
-      version='0.2',
-      description='Lightweight interface for SSRS reports to python',
+setup(name='sspyrs2',
+      version='1.0',
+      description='Alternative version of sspyrs that is a lightweight interface for SSRS reports to python.',
       long_description=open('README.rst').read(),
-      url='https://pypi.python.org/pypi/sspyrs',
-      author='James Nix',
-      author_email='james@nixanalytics.com',
+      url='https://pypi.python.org/pypi/sspyrs2',
+      author='Alex Sazo',
+      author_email='alexsazo@outlook.com',
       license='MIT',
-      packages=['sspyrs'],
+      packages=['sspyrs2'],
       install_requires=[
            'pandas>=0.18.1',
            'openpyxl>=2.4.7',
            'xmltodict>=0.10.2',
-           'requests_ntlm>=1.0.0'
+           'requests_ntlm>=1.0.0',
+           'beautifulsoup4>=4.8.2',
       ],
       keywords=['ssrs report reporting sql'],
       zip_safe=False)
